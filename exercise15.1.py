1068e5d0d043347576026d05f6f7435669b277f5
Philip Ulrich <philip@ulrich.life>
2017-10-17 21:46:27 +0000

    initial commit

---
+++ exercise15.1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import math
+import copy
+
+###############################################################################
+#
+print("\nExercise 15.1\n")
+#
+# Question 1
+# 1. Write a definition for a class named Circle with attributes center and 
+# radius, where center is a Point object and radius is a number.
+
+#
+# Question 2
+# 2. Instantiate a Circle object that represents a circle with its center at 
+# (150, 100) and radius 75.
+# 
+
+#
+# Question 3
+# 3. Write a function named point_in_circle that takes a Circle and a Point and 
+# returns True if the Point lies in or on the boundary of the circle.
+# 
+
+#
+# Question 4
+# 4. Write a function named rect_in_circle that takes a Circle and a Rectangle 
+# and returns True if the Rectangle lies entirely in or on the boundary of the 
+# circle.
+#
+
+# Question 5
+# 5. Write a function named rect_circle_overlap that takes a Circle and a 
+# Rectangle and returns True if any of the corners of the Rectangle fall inside 
+# the circle. Or as a more challenging version, return True if any part of the 
+# Rectangle falls inside the circle.
+#
